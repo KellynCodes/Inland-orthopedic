29b73a68d9a5edc2cf362465c32582944b188214
kellyncodes <kellyncodes@gmail.com>
2023-11-09 21:16:23 +0100

    chore: read more btn, continue reading indic

--- src/app/directives/truncate.directive.ts
+++ src/app/directives/truncate.directive.ts
@@ -26,7 +26,7 @@ export class TruncateDirective {
       const readMoreLink = document.createElement('a');
       readMoreLink.textContent = 'Read More';
       readMoreLink.className =
-        'cursor-pointer read-more text-pink-500 hover:text-pink-600';
+        'cursor-pointer inline-block bg-[#003367] hover:bg-[#ee6565e6] text-white rounded-full px-3 py-1 text-sm font-semibold mb-2';
       readMoreLink.addEventListener('click', () => {
         this.router.navigate(['about/detail']);
       });
