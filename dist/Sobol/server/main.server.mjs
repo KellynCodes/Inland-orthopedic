e51b283039119c26a69ca852e9cdfe747265f461
kellyncodes <kellyncodes@gmail.com>
2023-11-30 13:22:05 +0100

    custom deploy

---
+++ dist/Sobol/server/main.server.mjs
@@ -0,0 +1,10 @@
+import './polyfills.server.mjs';
+import {
+  main_server_default
+} from "./chunk-MZJF3ZWN.mjs";
+import "./chunk-2OTH3FXT.mjs";
+import "./chunk-MD6NWHS6.mjs";
+export {
+  main_server_default as default
+};
+//# sourceMappingURL=main.server.mjs.map
